5aa3aae25387afc594cd297ccfdf541c95f569c8
Meihua Li <meihua.1@hotmail.com>
2020-03-28 11:26:40 -0400

    Add files via upload

---
+++ Confirm.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react';
+import Dialog from '@material-ui/core/Dialog';
+import AppBar from '@material-ui/core/AppBar';
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import { List, ListItem, ListItemText } from '@material-ui/core/';
+import Button from '@material-ui/core/Button';
+
+export class Confirm extends Component {
+  continue = e => {
+    e.preventDefault();
+    // PROCESS FORM //
+    this.props.nextStep();
+  };
+
+  back = e => {
+    e.preventDefault();
+    this.props.prevStep();
+  };
+
+  render() {
+    const {
+      values: { firstName, lastName, email, date, time, location }
+    } = this.props;
+    return (
+      <MuiThemeProvider >
+        <React.Fragment>
+          <Dialog
+            open="true"
+            fullWidth="true"
+            maxWidth='sm'
+          >
+          <AppBar title="Confirm User Data" />
+          <List>
+            <ListItem>
+              <ListItemText primary="First Name" secondary={firstName} /> 
+            </ListItem>
+            <ListItem>
+              <ListItemText primary="Last Name" secondary={lastName} /> 
+            </ListItem>
+            <ListItem>
+              <ListItemText primary="Email" secondary={email} /> 
+            </ListItem>
+            <ListItem>
+              <ListItemText primary="Date" secondary={date} /> 
+            </ListItem>
+            <ListItem>
+              <ListItemText primary="Time" secondary={time} /> 
+            </ListItem>
+            <ListItem>
+              <ListItemText primary="Location" secondary={location} /> 
+            </ListItem>
+          </List>
+          <br />
+        
+          <Button
+            color="secondary"
+            variant="contained"
+            onClick={this.back}
+          >Back</Button>
+
+          <Button
+            color="primary"
+            variant="contained"
+            onClick={this.continue}
+          >Confirm & Continue</Button>
+          </Dialog>
+        </React.Fragment>
+      </MuiThemeProvider>
+    );
+  }
+}
+
+export default Confirm;
