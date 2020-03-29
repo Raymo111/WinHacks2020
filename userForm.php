85a9c7773fca2839f7af8c02ca4f9cf1a43b5f94
Raymond Li <hi@raymond.li>
2020-03-28 23:49:01 -0400

    Add form autocomplete

--- userForm.php
+++ userForm.php
@@ -24,6 +24,7 @@
 		<meta name="viewport" content="width=device-width">
 		<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.min.css" integrity="sha256-PHcOkPmOshsMBC+vtJdVr5Mwb7r0LkSVJPlPrp/IMpU=" crossorigin="anonymous" />
+		<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places"></script>
 		<script type="text/javascript" src="GPSData.js"></script>
 	</head>
 	<body>
@@ -179,6 +180,14 @@
 				document.getElementById("addedPlaces").removeChild(place);
 				document.getElementById("addedPlaces").removeChild(br);
 			}
+
+			var input = document.getElementById('place1');
+			var options = {
+				//types: ['establishment']
+			};
+
+			autocomplete = new google.maps.places.Autocomplete(input, options);
+
 		</script>
 		<div class="container">
 			<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
